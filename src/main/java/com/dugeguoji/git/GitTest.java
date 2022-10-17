8a5a53074b1665363d673742a16a4f0c24e3e605
adu <du227106@163.com>
2022-10-17 14:40:14 +0800

    test commit3

--- src/main/java/com/dugeguoji/git/GitTest.java
+++ src/main/java/com/dugeguoji/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("这只是测试一下idea使用git");
         System.out.println("这只是测试一下idea使用git2");
+        System.out.println("这只是测试一下idea使用git3");
     }
 }
