1859e43770d404160decf6521d5bf7614213d8ba
adu <du227106@163.com>
2022-10-17 14:39:24 +0800

    test commit2

--- src/main/java/com/dugeguoji/git/GitTest.java
+++ src/main/java/com/dugeguoji/git/GitTest.java
@@ -3,5 +3,6 @@ package com.dugeguoji.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("这只是测试一下idea使用git");
+        System.out.println("这只是测试一下idea使用git2");
     }
 }
